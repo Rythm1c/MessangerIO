4b93a5a42dff33b1801d433d08063a42a7d5d32a
Rythm1c <vickkaniu0@gmail.com>
2025-02-27 12:15:49 +0300

    adding messaging capability

--- script.js
+++ script.js
@@ -50,14 +50,10 @@ document.addEventListener("click", function (event) {
     }
 });
 
-class Contact {
-    constructor(name, contact, pfp, email) {
-        this.name = name;
-        this.contact = contact;
-        this.pfp = pfp;
-        this.email = email;
-    }
-}
+document.getElementById("close-modal").addEventListener("click", function () {
+    contactModal.classList.add("hidden");
+});
+
 
 class Message {
     constructor(time, text) {
@@ -68,19 +64,28 @@ class Message {
     find(value) {
         return this.text.contains(value);
     }
-
-
 }
 
-class Chat {
+class Contact {
     #messages;
-    constructor(sender, recipient) {
-        this.sender = sender;
-        this.recipient = recipient;
+    constructor(name, contact, pfp, email) {
+        this.name = name;
+        this.contact = contact;
+        this.pfp = pfp;
+        this.email = email;
+        this.#messages = []
+    }
+
+    get_contact_count() {
+        return this.#messages.length;
     }
 
     add_message(message) {
         this.#messages.push(message);
+    }
+
+    get_message(index) {
+        return this.#messages[index];
     }
 
     find(value) {
@@ -92,7 +97,62 @@ class Chat {
     }
 }
 
-/* const contacts = [];
-const Chat = []; */
+let Me = new Contact("Me", "0767566789", "none", "random@gmail.com");
 
+let contacts = [
+    new Contact("John Doe", "078982651", "none", "random@gmail.com"),
+    new Contact("Jane Smith", "078586661", "assets/jane.png", "random@gmail.com"),
+    new Contact("Alex Brown", "073445221", "assets/alex.jpeg", "random@gmail.com")
+];
+let current_contact = 0;
+
+function add_contact(contact) {
+    contacts.push(contact);
+}
+
+function display_texts() {
+    let chatContainer = document.getElementById("chat-container");
+    while (chatContainer.hasChildNodes()) {
+        chatContainer.removeChild(chatContainer.firstChild);
+    }
+    for (let i = 0; i < contacts[current_contact].get_contact_count(); ++i) {
+        chatContainer.appendChild(contacts[current_contact].get_message(i));
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+}
+
+function display_contact(index) {
+    current_contact = index;
+    display_texts();
+    document.getElementById("prof-name").innerHTML = contacts[current_contact].name;
+    if (contacts[current_contact].pfp === "none") {
+        document.getElementById("pfp").src = "assets/user.png";
+    }
+    else {
+        document.getElementById("pfp").src = contacts[current_contact].pfp;
+    }
+}
+
+display_contact(current_contact);
+
+document.getElementById("send-button").addEventListener("click", (event) => {
+    event.preventDefault();
+    let inputField = document.getElementById("chat-input");
+    let message = inputField.value.trim();
+    if (message !== "") {
+        let messageBubble = document.createElement("div");
+        messageBubble.classList.add("flex", "flex-col", "items-end");
+        messageBubble.innerHTML = `<div class='bg-blue-500 text-white p-2 rounded-[2px] max-w-xs'>${message}</div>`;
+        inputField.value = "";
+        contacts[current_contact].add_message(messageBubble);
+        display_texts()
+    }
+})
+
+document.querySelectorAll("#contact").forEach((contact, index) => {
+    contact.addEventListener("click", () => {
+        display_contact(index);
+        contactModal.classList.add("hidden");
+    });
+});
 
